5a516e605d7d4d5ef68c82e80b0f77ca9bae5ae2
Alyson van Hardenberg <avanhardenberg@gmail.com>
2015-09-26 14:01:19 -0700

    adding basic homepage and form handling

--- server.py
+++ server.py
@@ -1,14 +1,25 @@
+import os
+
 from flask import (Flask, render_template, request)
 
 from flask_debugtoolbar import DebugToolbarExtension
 
+from jinja2 import StrictUndefined
+
 app = Flask(__name__)
+app.secret_key = os.environ['FLASK_TOKEN']
 
 app.jinja_env.undefined = StrictUndefined
 
 @app.route('/')
 def home():
 	return render_template('home.html')
+
+@app.route('url_handler')
+def url_handler():
+	url = response.args.get('url')
+	print url
+	return url
 
 ####################################################
 
@@ -17,6 +28,7 @@ DEBUG = "NO_DEBUG" not in os.environ
 
 if __name__ == "__main__":
     # Set debug to true to have the toolbar extension run.
-    app.debug = DEBUG
-
-    app.run(host='0.0.0.0', port=PORT)
+    PORT = int(os.environ.get("PORT", 5000))
+    DEBUG = "NO_DEBUG" not in os.environ
+    
+    app.run(debug=DEBUG, host='0.0.0.0', port=PORT)
